d83f1db4a6e48ce5ab4d28dae03b242f2418a525
James Harkins <jh.harkins@protonmail.com>
2022-05-25 12:14:07 -0600

    Test: create tests for RailsEngineService #get_merchant_items class method

--- spec/services/rails_engine_service_spec.rb
+++ spec/services/rails_engine_service_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe RailsEngineService do
   describe "class methods" do
     describe "#get_all_merchants" do
-      it "returns an Hash containing a data key with a value of an Array all merchants" do
+      it "returns a Hash containing a data key with a value of an Array all merchants" do
         merchants = RailsEngineService.get_all_merchants
 
         expect(merchants).to be_a Hash
@@ -11,6 +11,21 @@ describe RailsEngineService do
         expect(merchants[:data][0]).to be_a Hash
         expect(merchants[:data][0][:attributes]).to be_a Hash
         expect(merchants[:data][0][:attributes][:name]).to eq("Schroeder-Jerde")
+      end
+    end
+
+    describe "#get_merchant_items" do
+      it "returns a Hash containing a data key with a value of an Array of all that merchants items" do
+        items = RailsEngineService.get_merchant_items(66)
+
+        expect(items).to be_a Hash
+        expect(items[:data]).to be_a Array
+        expect(items[:data][0]).to be_a Hash
+        expect(items[:data][:id]).to eq(1575)
+        expect(items[:data][0][:attributes]).to be_a Hash
+        expect(items[:data][0][:attributes][:name]).to eq("Item Corrupti Dignissimos")
+        expect(items[:data][0][:attributes][:description]).to eq("Sint non ipsum commodi mollitia veritatis. Autem et occaecati quo et quae reprehenderit eum. Harum aliquid aut a necessitatibus qui. Laudantium excepturi minima. Est nostrum sed laboriosam.")
+        expect(items[:data][0][:attributes][:unit_price]).to eq(623.74)
       end
     end
   end
