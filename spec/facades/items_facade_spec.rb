e39b4cea4d19efe3aa2391bbc7e45ae99ebeabe5
James Harkins <jh.harkins@protonmail.com>
2022-05-25 13:36:39 -0600

    Test: create tests for ItemsFacade #get_all_items class method

---
+++ spec/facades/items_facade_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe ItemsFacade do
+  describe "class methods" do
+    describe "#get_all_items" do
+      it "returns a collection of all Items as Item POROs" do
+        items = ItemsFacade.get_all_items
+
+        expect(items).to be_a Array
+        expect(items[0]).to be_an Item
+        expect(items.length).to eq(2483)
+      end
+    end
+  end
+end
