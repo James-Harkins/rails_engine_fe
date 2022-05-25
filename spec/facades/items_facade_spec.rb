54fa108a845ec0e336122cde587510389933c4ff
James Harkins <jh.harkins@protonmail.com>
2022-05-25 13:59:30 -0600

    Test: create test for ItemFacade #get_one_item class method

--- spec/facades/items_facade_spec.rb
+++ spec/facades/items_facade_spec.rb
@@ -11,5 +11,17 @@ describe ItemFacade do
         expect(items.length).to eq(2483)
       end
     end
+
+    describe "#get_one_item" do
+      it "returns one Item PORO based on item id" do
+        item = ItemFacade.get_one_item(4)
+
+        expect(item).to be_an Item
+        expect(item.id).to eq("4")
+        expect(item.name).to eq("Item Nemo Facere")
+        expect(item.description).to eq("Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.")
+        expect(item.unit_price).to eq(42.91)
+      end
+    end
   end
 end
